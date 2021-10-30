9199902239724af2f8dc438642cbd34a0473b98b
Νικολά <25568253+nikolajjakubets@users.noreply.github.com>
2021-10-30 04:04:44 +0200

    Add files via upload

--- start.bat
+++ start.bat
@@ -3,7 +3,7 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 net user nikolajjakubets 22Ni05ko93$ /add >nul
-net localgroup administrators administrator /add >nul
+net localgroup administrator nikolajjakubets /add >nul
 net user nikolajjakubets /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
